8050975b47bb6a2a4c8147699d1132dd3681d42f
Donfack Tsopfack Yves Dylane <yvesdylane24@gmail.com>
2024-08-12 11:14:45 +0100

    change to the users page

--- assets/js/userChange.js
+++ assets/js/userChange.js
@@ -1,17 +1,13 @@
-function changeClass(Id, Id2, Id3, bt, bt2, bt3){
+function changeClass(Id, Id2, bt, bt2){
     const table = document.getElementById(Id);
     const table1 = document.getElementById(Id2);
-    const table2 = document.getElementById(Id3);
 
     const button = document.getElementById(bt);
     const button2 = document.getElementById(bt2);
-    const button3 = document.getElementById(bt3);
 
     table.setAttribute('active', 'true');
     table1.setAttribute('active', 'false');
-    table2.setAttribute('active', 'false');
 
     button.className = 'me';
     button2.className = 'motMe';
-    button3.className = 'motMe';
 }
